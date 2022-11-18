cc0d1b337d50e3f21ee1d4e1c07d7c49ea567d7f
nicolas.stegmann <nicolas.stegmann@winiksoft.com>
2022-11-17 23:32:23 -0300

    firestore info

--- src/api/configFirestore.js
+++ src/api/configFirestore.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtAgF4W305ljOKB8T1EkzoGhDnHSE4oRA",
-  authDomain: "flowery4107.firebaseapp.com",
-  projectId: "flowery4107",
-  storageBucket: "flowery4107.appspot.com",
-  messagingSenderId: "387547875747",
-  appId: "1:387547875747:web:9a1f8674e1b8b0b6657216",
-  measurementId: "G-ZJGZSHKCYS"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
